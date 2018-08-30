7bf561e3d69b557622ab32a9ce371b13b82d8868
Andrew Bernier <a6bernier@gmail.com>
2018-08-30 22:22:51 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -8,6 +8,14 @@ def speak_to_grandma(phrase)
   end
 end
 
-
+def speak_to_grandma(phrase)
+  if phrase != phrase.upcase
+    return "HUH?! SPEAK UP SONNY!"
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  end
+end
 
 
